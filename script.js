4e18529036d9b0c177914318dbad3a23c963bbaf
iicoats <145803389+iicoats@users.noreply.github.com>
2023-12-01 21:21:37 -0600

    working on assignment

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header")
+    console.log("Selected header:",header);
+})
+
+const element = document.getElementById(main-container) 
+    element.style = black;
+
+
+const button = document.querySelectorAll(button);
+
+function newText  (newText) {
+    const element2 = document.getElementById(list-container) 
+}
